40ab99d1103b465bbd9e81f0522081b745e5b537
XD_TANGO <86399931+tanavurs@users.noreply.github.com>
2022-04-04 09:23:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+fabric.Image.fromURL('BirthdayImage.jpg', function(Img) {
+    block_Image_object = Img;
+    
+    block_Image_object.scaleToWidth(700);
+    block_Image_object.scaleToHeight(510);
+    block_Image_object.set({
+      top:0,
+      left:0  
+});
+canvas.add(block_Image_object);
+});
+}
+
+function playSound(){
+  x.play(); 
+}
